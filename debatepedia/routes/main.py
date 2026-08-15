06eb01c3eeb22efe1b28b6812b74a583f21e7e39
Large Tomas <bigtomcommisions@gmail.com>
2026-08-15 12:55:14 +0100

    added privacy and terms

--- debatepedia/routes/main.py
+++ debatepedia/routes/main.py
@@ -5,3 +5,11 @@ main = Blueprint('main', __name__)
 @main.get('/')
 def index():
     return render_template('index.html')
+
+@main.get('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@main.get('/terms')
+def terms():
+    return render_template('terms.html')
